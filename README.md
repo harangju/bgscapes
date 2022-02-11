f7324a84f6c406927206f87425203605699efb90
Harang Ju <wngkfkd94@gmail.com>
2022-02-11 18:07:41 -0500

    basic

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bg-scapes
 Menubar app for Punkscape desktop backgrounds.
+
+## To-Do
+1. Dark Mode
+2. Notifications
--- index.js
+++ index.js
@@ -1,17 +1,50 @@
-const { app, Menu, Tray } = require('electron');
+const { app, Menu, Tray, net } = require('electron');
 const path = require('path');
+const { exec } = require('child_process');
+const { menubar } = require('menubar');
+const cheerio = require('cheerio');
 
 const iconPath = path.join(__dirname, 'assets', 'images', 'icon.png');
 
 let tray = null;
+let mb = null;
 
 app.whenReady().then(() => {
-  tray = new Tray(iconPath);
-  const contextMenu = Menu.buildFromTemplate([
-		{ label: 'Live', type: 'radio', checked: true },
-    { label: 'Off', type: 'radio' },
-    { label: '', type: 'separator'},
-		{ label: 'Quit', type: 'normal' },
-	]);
-	tray.setContextMenu(contextMenu);
+  mb = menubar({
+    icon: iconPath,
+    showDocIcon: false,
+  });
+  mb.on('ready', () => {
+    console.log('Menubar app is ready.');
+    loadG27();
+    setBackground('~/Downloads/final-3.png');
+  });
 });
+
+const loadG27 = function() {
+  const urlG27 = 'https://punkscape.xyz/gallery27/now';
+  const request = net.request(urlG27);
+  request.on('response', (response) => {
+    console.log(`STATUS: ${response.statusCode}`);
+    response.on('data', (htmlString) => {
+      const $ = cheerio.load(htmlString);
+      console.log(`BODY: ${$('body')}`);
+    });
+    response.on('end', () => {
+      console.log('No more data in response.')
+    });
+  });
+  request.end();
+};
+
+const setBackground = function(imagePath) {
+  var script = "/usr/bin/osascript<<END\ntell application \"System Events\" to tell every desktop to set picture to \"" + imagePath + "\"\nEND"
+  exec(script,
+    function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    });
+};
